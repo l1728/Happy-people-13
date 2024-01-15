f61ce57909cc207a8455a79ca49f13dd9a28d67d
l1728 <cheburashka077@ukr.net>
2024-01-15 23:25:48 +0200

    killing aos

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-import AOS from 'aos';
-AOS.init({ once: true });
+
 let menuBtn = document.querySelector('.burger-btn');
 let menu = document.querySelector('.mobile-menu');
 let backdrop = document.querySelector('.mobile-menu-backdrop');
